0ec2579f050d904656cebf231a834a502a8e7e2f
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-11-05 15:18:38 +0100

    Updated plot example_pattern_recon

--- 5_Results/r01_results_example_pattern_reconstruction.R
+++ 5_Results/r01_results_example_pattern_reconstruction.R
@@ -10,7 +10,10 @@
 #### 1. Import packages and data ####
 
 # Packages #
+library(dplyr)
 library(RColorBrewer)
+library(SHAR)
+library(spatstat)
 library(tidyverse)
 
 #### 1. Input data ####
@@ -27,68 +30,74 @@ names_combined <- paste0(names_split[, 2], "_", names_split[, 3], "_", names_spl
 names(results) <- names_combined
 
 ### 2. Calculate pcf ####
-
 results_df_long <- purrr::map_dfr(results, function(current_species) {
   purrr::map_dfr(current_species, function(current_pattern) { 
     tibble::as.tibble(
-      as.data.frame(
       spatstat::pcf.ppp(current_pattern, 
                         divisor = "d", 
                         correction = "Ripley", 
                         r = seq(from = 0, to = 100, length.out = 515))
-      )
     )
   }, .id = "pattern")
 }, .id = "species")
 
-results_df_long_reclassified <- results_df_long %>%
-  dplyr::mutate(pattern = dplyr::case_when(pattern == "observed" ~ "observed", 
-                                           pattern != "observed" ~ "simulation"), 
-                method = dplyr::case_when(str_detect(species, "^reconstruction") ~ "Pattern reconstruction",
-                                          str_detect(species, "^fitted") ~ "Gamma test"), 
-                species = dplyr::case_when(str_detect(species, "species_1") ~ "species_1",
-                                           str_detect(species, "species_2") ~ "species_2", 
-                                           str_detect(species, "species_3") ~ "species_3", 
-                                           str_detect(species, "species_4") ~ "species_4"))
+results_df_long_reclassified <- dplyr::mutate(results_df_long, 
+                                              pattern = dplyr::case_when(pattern == "observed" ~ "observed", 
+                                                                         pattern != "observed" ~ "simulation"), 
+                                              method = dplyr::case_when(pattern == "observed" ~ "Observed",
+                                                                        str_detect(species, "^reconstruction") ~ "Pattern reconstruction",
+                                                                        str_detect(species, "^fitted") ~ "Gamma test"), 
+                                              species = dplyr::case_when(str_detect(species, "species_1") ~ "species_1",
+                                                                         str_detect(species, "species_2") ~ "species_2", 
+                                                                         str_detect(species, "species_3") ~ "species_3", 
+                                                                         str_detect(species, "species_4") ~ "species_4"))
 
-pcf_observed <- results_df_long_reclassified %>%
-  dplyr::filter(pattern == "observed" & 
-                species == "species_1" | 
-                species == "species_2")
+pcf_observed <- dplyr::filter(results_df_long_reclassified, 
+                              pattern == "observed")
 
 pcf_observed$species <- factor(pcf_observed$species, 
-                                levels = c("species_1",
-                                           "species_2"), 
+                               levels = c("species_1",
+                                          "species_2", 
+                                          "species_3",
+                                          "species_4"), 
                                labels = c("Species 1", 
-                                          "Species 2"))
+                                          "Species 2", 
+                                          "Species 3",
+                                          "Species 4"))
 
-pcf_null_model <- results_df_long_reclassified %>%
-  dplyr::filter(pattern == "simulation" & 
-                species == "species_1" | 
-                species == "species_2") %>% 
+pcf_null_model <- dplyr::filter(results_df_long_reclassified, 
+                                pattern == "simulation") %>% 
   dplyr::group_by(species, method, r) %>% 
   dplyr::summarise(lo = quantile(iso, probs = 0.025),
                    hi = quantile(iso, probs = 0.975))
+
+pcf_null_model$species <- factor(pcf_null_model$species, 
+                                 levels = c("species_1",
+                                            "species_2", 
+                                            "species_3",
+                                            "species_4"), 
+                                 labels = c("Species 1", 
+                                            "Species 2", 
+                                            "Species 3",
+                                            "Species 4"))
 
 pcf_null_model$method <- factor(pcf_null_model$method, 
                                 levels = c("Gamma test",
                                            "Pattern reconstruction"))
 
 #### 3. Plot results ###
-
-colors_spec <- rev(RColorBrewer::brewer.pal(n = 3, name = "Spectral"))
-
-plot_method_comparison <- ggplot(data = pcf_observed) + 
-  geom_ribbon(data = pcf_null_model, aes(x = r, ymin = lo, ymax = hi, fill = "Null model")) +
-  geom_line(aes(x = r, y = theo, color = "Complete spatial randomness"), linetype = 2, size = 1.25) +
-  geom_line(aes(x = r, y = iso, color = "Observed"), size = 1.25) +
-  scale_color_manual(name = "", values = c("Complete spatial randomness" = colors_spec[1], "Observed" = colors_spec[3])) +
-  scale_fill_manual(name = "", values = c("Null model" = "grey")) +
-  facet_wrap(~ method + species, nrow = 2, ncol = 2) +
+plot_method_comparison <- ggplot(data = pcf_null_model) + 
+  geom_ribbon(data = pcf_null_model, aes(x = r, ymin = lo, ymax = hi, fill = method)) +
+  geom_line(data = pcf_observed, aes(x = r, y = theo, linetype = "Complete spatial randomness"), size = 1, col = "grey35") +
+  geom_line(data = pcf_observed, aes(x = r, y = iso, linetype = "Observed"), size = 1, col = "grey35") +
+  scale_linetype_manual(name = "", values = c("Observed" = 1, "Complete spatial randomness" = 2)) +
+  scale_fill_viridis_d(name = "") +
+  facet_wrap(~ species) +
   labs(x = "r [m]", y = "g(r)") +
-  theme_classic(base_size = 30) +  
+  theme_classic(base_size = 15) +  
   guides(color = guide_legend(nrow = 2, byrow = TRUE)) +
-  theme(legend.position="bottom")
+  theme(legend.position = "bottom", 
+        legend.key.width = unit(1.5, "cm"))
 
 #### 4. Save result ####
 UtilityFunctions::save_ggplot(plot = plot_method_comparison, 
